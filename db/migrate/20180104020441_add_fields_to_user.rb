c23beb5511b7906d3db4ec621d0324b2a7a96d93
Andrew Liwanag <drutyper@gmail.com>
2018-01-04 16:56:34 -0500

    Added username to sign up option, added logout functionality

--- db/migrate/20180104020441_add_fields_to_user.rb
+++ db/migrate/20180104020441_add_fields_to_user.rb
@@ -1,5 +1,6 @@
 class AddFieldsToUser < ActiveRecord::Migration[5.1]
   def change
+    add_column :users, :username, :string, unique: true
     add_column :users, :first_name, :string
     add_column :users, :last_name, :string
     add_column :users, :cell_number, :string
